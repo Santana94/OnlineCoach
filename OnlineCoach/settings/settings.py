4a39715ce14fe69436f1f00ef6734600ce8614b6
Matheus Sant'ana <matheusm.santana@outlook.com>
2021-10-01 14:15:24 -0300

    database settings correction.

--- OnlineCoach/settings/settings.py
+++ OnlineCoach/settings/settings.py
@@ -15,20 +15,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
-
-# TODO: Change database to Postgres
-# Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE':   'django.db.backends.oracle',
-#         'NAME':     os.environ['DATABASE_DEFAULT_NAME'],
-#         'USER':     os.environ['DATABASE_DEFAULT_USER'],
-#         'PASSWORD': os.environ['DATABASE_DEFAULT_PASSWORD'],
-#     }
-# }
 
 # TODO: Insert Sentry configuration
