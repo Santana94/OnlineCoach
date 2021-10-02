cfa3cc09e7e5fb6e0323832bde8e61be759a0043
Matheus Sant'ana <matheusm.santana@outlook.com>
2021-10-02 10:50:36 -0300

    muscle mass insertion

--- user_profile/views.py
+++ user_profile/views.py
@@ -40,11 +40,26 @@ class DetailUserProfile(DetailView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         body_measures = BodyMeasures.objects.filter(user_profile_id=self.object.pk)
+
+        labels = []
+        ffmi = []
+        weight = []
+        body_fat = []
+        muscle_mass = []
+        for body_measure in body_measures:
+            labels.append(body_measure.measurement_date.strftime("%B %d, %Y"))
+            ffmi.append(round(body_measure.ffmi, 1))
+            weight.append(round(body_measure.weight, 1))
+            body_fat.append(round(body_measure.body_fat_percentage, 1))
+            muscle = body_measure.weight * (1 - body_measure.body_fat_percentage/100)
+            muscle_mass.append(round(muscle))
+
         context_data['progress_data'] = body_measures
-        context_data['labels'] = [i.measurement_date.strftime("%B %d, %Y") for i in body_measures]
-        context_data['ffmi'] = [round(i.ffmi, 1) for i in body_measures]
-        context_data['weight'] = [round(i.weight, 1) for i in body_measures]
-        context_data['body_fat'] = [round(i.body_fat_percentage, 1) for i in body_measures]
+        context_data['labels'] = labels
+        context_data['ffmi'] = ffmi
+        context_data['weight'] = weight
+        context_data['body_fat'] = body_fat
+        context_data['muscle_mass'] = muscle_mass
         return context_data
 
 
